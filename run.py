b6104d385234dde3b38497196062de803f393ce9
Yunato <yunato525@gmail.com>
2019-10-31 14:24:10 +0900

    Add feedparser lib

--- run.py
+++ run.py
@@ -1,8 +1,18 @@
+import feedparser
 from slackbot.bot import Bot
 
+def getFeed(rss_url):
+    for entry in feedparser.parse(rss_url).entries:
+        print(entry.title)
+        print(entry.link)
+        print(entry.category)
+        print('----')
+    
+
 def main():
-    bot = Bot()
-    bot.run()
+    # bot = Bot()
+    # bot.run()
+    getFeed(RSS_URL)
 
 if __name__ == "__main__":
     main()
