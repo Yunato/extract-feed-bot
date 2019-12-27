21ea01d64e812ffcf262f88b190670d8ddd2b0c0
Yunato <yunato525@gmail.com>
2019-12-27 23:01:32 +0900

    Fix Procfile and run.py for regular execution

--- bot/run.py
+++ bot/run.py
@@ -1,11 +1,26 @@
+import os
+import urllib.request
+import sys
 from slackbot.bot import Bot
 from bot.controller import Controller
 
-RSS_URL = "example.com"
-
 def main():         
-    bot = Bot()
-    bot.run()
+    args = sys.argv
+    if args[1] == "bot":
+        bot = Bot()
+        bot.run()
+    elif args[1] == "fetch":
+        fetch()
+    elif args[1] == "send":
+        send()
+
+def fetch():
+    Controller().fetch_feed()
+
+def send():
+    controller = Controller()
+    req = urllib.request.Request(os.environ.get('WEBHOOK_URL'), controller.create_message())
+    urllib.request.urlopen(req)
 
 if __name__ == "__main__":
     main()
--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -1,21 +1,9 @@
 import json
-import urllib.request
-import os
 from slackbot.bot import listen_to
 from slackbot.bot import respond_to
 from bot.controller import Controller
 
 controller = Controller()
-
-@listen_to('fetch')
-def fetch(message):
-    controller.add_url("Alice", "https://gigazine.net/index.php?/news/rss_2.0/")
-    message.send('get {} feeds'.format(controller.fetch_feed()))
-
-@listen_to('send')
-def send(message):
-    req = urllib.request.Request(os.environ.get('WEBHOOK_URL'), controller.create_message())
-    urllib.request.urlopen(req)
 
 @respond_to('(.*)')
 def exec_command_with_one_arg(message, args):
